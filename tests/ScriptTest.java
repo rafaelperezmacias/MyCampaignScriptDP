3a42c8e0efadea10f290d265959a5c693d4c5c51
= <rafaelpemaceti25@gmail.com>
2022-11-02 23:35:07 -0600

    Se agrego la creacion de archivos, falta modificar el tamaño del batch de carga y convertirlo a script

--- tests/ScriptTest.java
+++ tests/ScriptTest.java
@@ -9,10 +9,10 @@ import java.util.StringTokenizer;
 public class ScriptTest {
 
     private static ArrayList<State> states = new ArrayList<>();
-    private static int federalDistricts = 1;
-    private static int localDistricts = 1;
-    private static int municipalities = 1;
-    private static int sections = 1;
+    private static int federalDistricts = 0;
+    private static int localDistricts = 0;
+    private static int municipalities = 0;
+    private static int sections = 0;
 
     public static void main(String[] args) {
         // Apertura del archivo
@@ -43,23 +43,23 @@ public class ScriptTest {
                 StringTokenizer tokenizer = new StringTokenizer(line, ",");
                 // Informacion del estado
                 State newState = new State();
-                newState.setId(Integer.parseInt(tokenizer.nextToken()));
-                newState.setName(tokenizer.nextToken());
+                newState.setId(Integer.parseInt(tokenizer.nextToken().trim()));
+                newState.setName(tokenizer.nextToken().trim());
                 // Informacion del distrito federal
                 FederalDistrict newFederalDistrict = new FederalDistrict();
-                newFederalDistrict.setNumber(Integer.parseInt(tokenizer.nextToken()));
-                newFederalDistrict.setName(tokenizer.nextToken());
+                newFederalDistrict.setNumber(Integer.parseInt(tokenizer.nextToken().trim()));
+                newFederalDistrict.setName(tokenizer.nextToken().trim());
                 // Informacion del distrito local
                 LocalDistrict newLocalDistrict = new LocalDistrict();
-                newLocalDistrict.setNumber(Integer.parseInt(tokenizer.nextToken()));
-                newLocalDistrict.setName(tokenizer.nextToken());
+                newLocalDistrict.setNumber(Integer.parseInt(tokenizer.nextToken().trim()));
+                newLocalDistrict.setName(tokenizer.nextToken().trim());
                 // Informacion del municipio
                 Municipality newMunicipality = new Municipality();
-                newMunicipality.setNumber(Integer.parseInt(tokenizer.nextToken()));
-                newMunicipality.setName(tokenizer.nextToken());
+                newMunicipality.setNumber(Integer.parseInt(tokenizer.nextToken().trim()));
+                newMunicipality.setName(tokenizer.nextToken().trim());
                 // Informacion de la seccion
                 Section newSection = new Section();
-                newSection.setSection(Integer.parseInt(tokenizer.nextToken()));
+                newSection.setSection(Integer.parseInt(tokenizer.nextToken().trim()));
                 // Tratamiento para los datos
                 // Estado
                 boolean insertState = true;
@@ -83,7 +83,7 @@ public class ScriptTest {
                     }
                 }
                 if ( insertFederalDistrict ) {
-                    newFederalDistrict.setId(federalDistricts++);
+                    newFederalDistrict.setId(++federalDistricts);
                     newState.getFederalDistricts().add(newFederalDistrict);
                 }
                 // Distrito local
@@ -96,7 +96,7 @@ public class ScriptTest {
                     }
                 }
                 if ( insertLocalDistrict ) {
-                    newLocalDistrict.setId(localDistricts++);
+                    newLocalDistrict.setId(++localDistricts);
                     newState.getLocalDistricts().add(newLocalDistrict);
                 }
                 // Municipio
@@ -109,7 +109,7 @@ public class ScriptTest {
                     }
                 }
                 if ( insertMunicipality ) {
-                    newMunicipality.setId(municipalities++);
+                    newMunicipality.setId(++municipalities);
                     newState.getMunicipalities().add(newMunicipality);
                 }
                 // Seccion
@@ -122,7 +122,7 @@ public class ScriptTest {
                     }
                 }
                 if ( insertSecction ) {
-                    newSection.setId(sections++);
+                    newSection.setId(++sections);
                     newSection.setState(newState);
                     newSection.setMunicipality(newMunicipality);
                     newSection.setLocalDistrict(newLocalDistrict);
@@ -148,6 +148,10 @@ public class ScriptTest {
 
         // Generacion de los archivos txt para las inserciones
         generateFileForStates();
+        generateFileForFederalDistricts();
+        generateFileForLocalDistricts();
+        generateFileForMunicipalities();
+        generateFileForSections();
 
         System.out.println("Lineas leidas: " + lines);
         System.out.println();
@@ -171,12 +175,73 @@ public class ScriptTest {
 
     private static void generateFileForStates() {
         StringBuilder builder = new StringBuilder();
-        builder.append("INSERT INTO `state`(`id`, `name`) VALUES ").append("\n");
+        builder.append("INSERT INTO `states`(`id`, `name`) VALUES ").append("\n");
         for ( State state : states ) {
-            builder.append("(").append(state.getId()).append(", '").append(state.getName()).append("'), ").append("\n");
+            builder.append("(").append(state.getId()).append(",'").append(state.getName()).append("'), ").append("\n");
         }
         builder.replace(builder.length() - ", \n".length(), builder.length(), ";");
         writeToDisk(builder.toString(), "states.txt");
+    }
+
+    private static void generateFileForFederalDistricts() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("INSERT INTO `federal_districts`(`id`, `name`, `number`) VALUES ").append("\n");
+        for ( State state : states ) {
+            for ( FederalDistrict federalDistrict : state.getFederalDistricts() ) {
+                builder.append("(").append(federalDistrict.getId()).append(",'").append(federalDistrict.getName());
+                builder.append("', ").append(federalDistrict.getNumber()).append("), ").append("\n");
+            }
+        }
+        builder.replace(builder.length() - ", \n".length(), builder.length(), ";");
+        writeToDisk(builder.toString(), "federalDistricts.txt");
+    }
+
+    private static void generateFileForLocalDistricts() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("INSERT INTO `local_districts`(`id`, `name`, `number`) VALUES ").append("\n");
+        for ( State state : states ) {
+            for ( LocalDistrict localDistrict : state.getLocalDistricts() ) {
+                builder.append("(").append(localDistrict.getId()).append(",'").append(localDistrict.getName());
+                builder.append("', ").append(localDistrict.getNumber()).append("), ").append("\n");
+            }
+        }
+        builder.replace(builder.length() - ", \n".length(), builder.length(), ";");
+        writeToDisk(builder.toString(), "localDistricts.txt");
+    }
+
+    private static void generateFileForMunicipalities() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("INSERT INTO `municipalities`(`id`, `name`, `number`) VALUES ").append("\n");
+        for ( State state : states ) {
+            for ( Municipality municipality : state.getMunicipalities() ) {
+                builder.append("(").append(municipality.getId()).append(",'").append(municipality.getName());
+                builder.append("', ").append(municipality.getNumber()).append("), ").append("\n");
+            }
+        }
+        builder.replace(builder.length() - ", \n".length(), builder.length(), ";");
+        writeToDisk(builder.toString(), "municipalities.txt");
+    }
+
+    private static void generateFileForSections() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("INSERT INTO `sections`(`id`, `section`, `state_id`, `municipality_id`, `federal_district_id`, `local_district_id`) VALUES ").append("\n");
+        int row = 0;
+        for ( State state : states ) {
+            for ( Section section : state.getSections() ) {
+                builder.append("(").append(section.getId()).append(", '").append(section.getSection()).append("', ");
+                builder.append(state.getId()).append(", ").append(section.getMunicipality().getId()).append(", ");
+                builder.append(section.getFederalDistrict().getId()).append(", ").append(section.getLocalDistrict().getId());
+                builder.append("), ").append("\n");
+                if ( ++row == 2500 ) {
+                    builder.replace(builder.length() - ", \n".length(), builder.length(), ";");
+                    builder.append("\n");
+                    builder.append("INSERT INTO `sections`(`id`, `section`, `state_id`, `municipality_id`, `federal_district_id`, `local_district_id`) VALUES ").append("\n");
+                    row = 0;
+                }
+            }
+        }
+        builder.replace(builder.length() - ", \n".length(), builder.length(), ";");
+        writeToDisk(builder.toString(), "sections.txt");
     }
 
     private static void writeToDisk(String text, String path) {
